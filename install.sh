f03b74a6a35268bb72b516b98791ee8b78c34c98
KrustyHack <KrustyHack@users.noreply.github.com>
2020-03-27 17:57:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,3 @@ mkdir /media/script && mount -t tmpfs -o size=1m tmpfs /media/script
 wget -P /media/script https://raw.githubusercontent.com/krustyhack/Linux-to-Windows-with-QEMU/master/mediabots_Ubuntu-to-Windows.sh
 
 chmod +x /media/script/*
-
-/media/script/mediabots_Ubuntu-to-Windows.sh
